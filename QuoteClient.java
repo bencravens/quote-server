6f984b9d3fbdc9a085c21b5e53a4a304b6dc40c3
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2021-08-31 15:49:45 +1200

    Add multithreading

--- QuoteClient.java
+++ QuoteClient.java
@@ -3,6 +3,7 @@
 
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class QuoteClient {
 
@@ -11,16 +12,35 @@ public class QuoteClient {
         String hostname = "localhost";
         int port = 5000;
 
+        //connect to the server
         try (Socket sock = new Socket(hostname, port)) {
             System.out.println("Connected to " + hostname + " on port " + port);
+            //writing to server
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+            //reading from server
             InputStream input = sock.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            
+            // scan for user input to send to server
+            Scanner sc = new Scanner(System.in);
+            String line = null;
+            //if the user types "exit", quit.
+            while (!"exit".equalsIgnoreCase(line)) {
+                //read from user
+                line = sc.nextLine();
 
-            String quote = reader.readLine();
+                //send user input to server
+                out.println(line);
+                out.flush();
 
-            System.out.println(quote);
+                //display server reply
+                String quote = reader.readLine();
+                System.out.println(quote);
+            }
+            //close scanner
+            sc.close();
         } catch (Exception e) {
             System.out.println(e);
-        }   
-    }   
+        } 
+    }
 }
--- QuoteServer.java
+++ QuoteServer.java
@@ -1,6 +1,6 @@
 // Based on the tutorial at 
 // www.codejava.net/java-se/networking/java-socket-server-examples-tcp-ip 
-
+// and https://www.geeksforgeeks.org/multithreaded-servers-in-java/#:~:text=Multithreaded%20Server%3A%20A%20server%20having,clients%20at%20the%20same%20time.
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
@@ -13,29 +13,64 @@ class QuoteServer {
 
         System.out.println("Initializing server");
         int port = 5000;
-
+         
         try {
             ServerSocket serversock = new ServerSocket(port);
             System.out.println("Server is listening on port " + port);
-
+            
             while (true) {
-                Socket sock = serversock.accept();
+                Socket client = serversock.accept();
                 System.out.println("New client connected");
-                OutputStream output = sock.getOutputStream();
-                PrintWriter writer = new PrintWriter(output, true);
-                String quote = readQuote();
-                writer.println(quote);
+                //add new thread with connected client
+                ClientHandler clientsock = new ClientHandler(client);
+                new Thread(clientsock).start();
             }
-
+        
         } catch (Exception e) {
+            System.out.println("Error connecting to client");
             System.out.println(e);
         }
     }
 
+    // Spawn a thread and send quote to client
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+
+        //constructor
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        //run is called when a new thread is made
+        public void run() {
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+                // create inputstream to get input from client
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // create outputstream to send quote to client
+                OutputStream output = clientSocket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+                // if the client sends us something, send them back a quote
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.out.printf("Client: %s\n",line);
+                    String quote = readQuote();
+                    writer.println(quote);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error getting output stream");
+                System.out.println(e);
+            }
+        }
+    }
+
+    //Read a quote randomly from the quote file 
     public static String readQuote() {
         Random random = new Random();
         long num_lines = 0;
-        Path path = Paths.get("quotes.txt");
+        Path path = Paths.get("quotes.txt"); 
         try {
             num_lines = Files.lines(path).count();
         } catch (IOException e) {
@@ -43,7 +78,7 @@ class QuoteServer {
         }
 
         // pick a random quote
-        int randomInteger = random.nextInt((int)num_lines);
+        int randomInteger = random.nextInt((int)num_lines); 
         try {
             String quote = Files.readAllLines(path).get(randomInteger);
             return quote;
@@ -53,3 +88,4 @@ class QuoteServer {
         }
     }
 }
+
